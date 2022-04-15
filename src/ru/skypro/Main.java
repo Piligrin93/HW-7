d5ec7b9e55ccc0da91d875e77957e89ae81f0edb
Vladimir Mokhov <mohov_vova@mail.u>
2022-04-15 20:51:20 +0300

    Second commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,31 @@
 package ru.skypro;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+	// Задание 1 и Задание 2
+
+        String firstName = "Ivan "; // Объявляем три строки: имя, отчество и фамилия
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName; // С помощью конкатенации объединяем строки
+        System.out.println(fullName);
+
+        String fullNameUp = fullName.toUpperCase(); // С помощью метода toUpperCase все символы строки fullName перейдут в верхний регистр
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUp);
+
+        StringBuilder fullNameNew = new StringBuilder("Ivanov Ivan Ivanovich");
+        fullNameNew.delete(7, ' ');
+        fullNameNew.append("Семён Семёнович");
+        System.out.println(fullNameNew);
+
+
+        fullName = "Иванов Семён Семёныч";
+
+        String fullNameReplace = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника " + fullNameReplace);
+
+        }
 }
